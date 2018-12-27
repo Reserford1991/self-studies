c76d409c0054aa32210b1888a06ffb5b65cc7d63
Reserford1991 <vitaliymatveev1991@gmail.com>
2018-12-27 23:45:05 +0300

    add all nessessary components to course-project

---
+++ course-project/src/app/recipes/resipes-detail/resipes-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resipes-detail',
+  templateUrl: './resipes-detail.component.html',
+  styleUrls: ['./resipes-detail.component.css']
+})
+export class ResipesDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ course-project/src/app/recipes/resipes-list/resipes-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resipes-list',
+  templateUrl: './resipes-list.component.html',
+  styleUrls: ['./resipes-list.component.css']
+})
+export class ResipesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
